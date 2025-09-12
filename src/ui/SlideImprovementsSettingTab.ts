bb755498accef119e4991837a49655393a712b6c
Jared Cherup <88674187+jarbochov@users.noreply.github.com>
2025-09-12 18:20:39 -0400

    Revert "Trying recompile. "
    This reverts commit 79b3135bbcb921e44d7ffe5ea0616919d9c65b22.

--- src/ui/SlideImprovementsSettingTab.ts
+++
@@ -1,7 +0,0 @@
-import { PluginSettingTab, App, Setting } from "obsidian";
-import { SlideImprovementsSettings } from "../types";
-import { ObsidianSlideImprovementsPlugin } from "../main"; // adjust import as needed
-
-export class SlideImprovementsSettingTab extends PluginSettingTab {
-  // ...full class code...
-}
